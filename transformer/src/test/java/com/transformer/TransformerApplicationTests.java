55b578e1e8e603cb48e5e71ac8a85e1a4d1eb56c
Nikos Kougianos <kougi_jr@hotmail.com>
2020-02-21 10:08:42 +0200

    Generate maven projects - Receiver and Transformer microservice

---
+++ transformer/src/test/java/com/transformer/TransformerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transformer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransformerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
